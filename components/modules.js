62e0ca1c360bec9f633b00168bf74ef60b116d95
sKzAy <1mkzaexe@gmail.com>
2024-10-12 19:42:44 +0500

    Finale mayubee

--- components/modules.js
+++ components/modules.js
@@ -6,16 +6,16 @@ const Modules = () => {
     <div  id='modules' className='flex1'>
       <div className=" about-event flex1 mt-10 ">
     <div className="mod-div text-white border-2 border-r-4 border-red-600 rounded-xl p-10">
-      <h2 className='font-extrabold text-2xl text-yellow-500'>Modules:</h2>
+      <h2 className='font-extrabold text-2xl text-yellow-500 pb-4 size-large'>Modules:</h2>
         <ul>
-            <li>The Euler Expedition (Maths)</li>
-            <li> The Musk Marketplace (Business)</li>
-            <li> The Sherlock Solution (Law & Crime)</li>
-            <li>The Hawking Hypothesis (Physics)</li>
-            <li> The Gates Gateway (Computer Science)</li>
-            <li> The Edison Edit (Media)</li>
-            <li>The Picasso Puzzle (Art)</li>
-            <li> The da Vinci Dare (Surprise Module)</li>
+            <li className='size-small'>The Euler Expedition (Maths)</li>
+            <li className='size-small'> The Musk Marketplace (Business)</li>
+            <li className='size-small'> The Sherlock Solution (Law & Crime)</li>
+            <li className='size-small'>The Hawking Hypothesis (Physics)</li>
+            <li className='size-small'> The Gates Gateway (Computer Science)</li>
+            <li className='size-small'> The Edison Edit (Media)</li>
+            <li className='size-small'>The Picasso Puzzle (Art)</li>
+            <li className='size-small'> The da Vinci Dare (Surprise Module)</li>
         </ul>
     </div>
      </div>
--- components/registration.js
+++ components/registration.js
@@ -6,19 +6,18 @@ const Registration = () => {
     return (
         <>
             <div id='registration' className="about-event flex1 mt-10 ">
-                <div className=" mod-div text-white rounded-xl border-2 border-red-500 p-10"><h2 className='font-extrabold text-2xl text-yellow-500' id='modules'>Registration:</h2>
-                    <h3 className='text-red-500'>Registration fee: </h3>
+                <div className=" mod-div text-white rounded-xl border-2 border-red-500 p-10"><h2 className='font-extrabold text-2xl text-yellow-500' id='modules size-large'>Registration:</h2>
+                    <h3 className='text-red-500 pb-4 pt-4 size-mid'>Registration fee: </h3>
                     <ul>
-                        <li> Fee Per Delegate: Rs.2000+2000 (Early Bird)</li>
-                        <li>Fee Per Delegate: Rs.2500 + 2500 (Normal Registrations)</li>
-                        <li></li>
+                        <li className='size-small'> Fee Per Delegate: Rs.2000+2000 (Early Bird)</li>
+                        <li className='size-small'>Fee Per Delegate: Rs.2500 + 2500 (Normal Registrations)</li>
                     </ul>
-                    <h3 className='text-red-500'>Registration Details:</h3>
+                    <h3 className='text-red-500 pt-4 pb-4 size-mid'>Registration Details:</h3>
                     <ul>
-                        <li> Each delegation would comprise of 2-6 delegates</li>
-                        <li>Registrations open: 11th October</li>
-                        <li>Early Bird Discount ends:22nd October</li>
-                        <li>Registrations close: 31st October</li>
+                        <li className='size-small'> Each delegation would comprise of 2-6 delegates</li>
+                        <li className='size-small'>Registrations open: 11th October</li>
+                        <li className='size-small'>Early Bird Discount ends:22nd October</li>
+                        <li className='size-small'>Registrations close: 31st October</li>
                     </ul>
                     <div className="btn">
                     {/* <Link href="www.google.com">
