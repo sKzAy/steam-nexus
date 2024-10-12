3f707c0aec437654d5c16ba5fc353c6ed4cff06b
sKzAy <1mkzaexe@gmail.com>
2024-10-12 20:36:28 +0500

    10/12/2024 done for this day

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import react from "react";
 import Navbar from "@/components/Navbar";
 import MainContent from "@/components/main";
+import Footer from "@/components/footer";
 <link rel="stylesheet" href="globals.css" />
 import "./globals.css"
 
@@ -9,8 +10,11 @@ export default function Home() {
 
   return (
   <>
+  <div class="absolute top-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
   <div><Navbar/></div>
   <div><MainContent/></div>
+  <div><Footer/></div>
+  </div>
   </>
   );
 }
---
+++ components/footer.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex1 mt-10'>
+         <div className="bottom-fixed text-white text-center w-full ">
+        Made by <a className='underline hover:text-lg ' target='_blank' href="https://www.instagram.com/s_kazimali_/profilecard/?igsh=MTNzcnJieXA5MTd1Zg==">Kazim Ali</a> 
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
+
