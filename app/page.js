c58120493df0a68e5408e8db3b28dddb737c7f87
sKzAy <1mkzaexe@gmail.com>
2024-10-12 11:03:21 +0500

    tryna fix bro

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
   <>
   <Head> </Head>
-  <div class=" -z-10 absolute top-0  min-h-full w-max bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+  <div class=" -z-10 absolute top-0  min-h-full min-w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
   <div><Navbar/></div>
   <div><MainContent/></div>
 
--- components/main.js
+++ components/main.js
@@ -15,8 +15,8 @@ const MainContent = () => {
         <div><Card/></div>
 
         <div className="quote text-white flex justify-center align-middle">
-        <p className='text-yellow-500'>"Education is not the learning of facts, but the training of the mind to think."
-        </p><p className=' font-bold text-red-500 '>-Albert Einstein</p></div>
+        <p className='text-yellow-500'>&quot;Education is not the learning of facts, but the training of the mind to think.&quot;
+        </p><p className=' font-bold text-red-500 pl-3'>-Albert Einstein</p></div>
     </div>
     <div className="about-event flex justify-center align-middle mt-10 ">
     <div className=" text-white w-[40vw] border-4 border-r-4 border-red-600 rounded-xl p-10"><h2 className='font-extrabold text-2xl text-yellow-500'>Event Details:</h2><p>Potohar Spark Fest aims to foster a culture of innovation and hands-on learning at Beaconhouse Potohar Campus by empowering students to apply theoretical STEM concepts to real-world challenges. As the college's inaugural event of its kind, it seeks to inspire creativity, critical thinking, and collaboration, preparing a new generation of problem-solvers for a dynamic technological landscape. Collaborating with professionals from organizations like NICAT and the NUST Science Society, the fest will offer interactive workshops, challenges, and competitions that encourage deep thinking and innovative application of knowledge, embodying the philosophy that true education transcends rote memorization.</p></div></div>
