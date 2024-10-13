777e3b34c8f2af01b303a24e65447722657d52cb
sKzAy <1mkzaexe@gmail.com>
2024-10-13 12:18:52 +0500

    favicon icon

--- app/page.js
+++ app/page.js
@@ -4,12 +4,14 @@ import MainContent from "@/components/main";
 import FooterComponent from "@/components/footer";
 <link rel="stylesheet" href="globals.css" />
 import "./globals.css"
+import Head from "next/head";
 
 export default function Home() {
   
 
   return (
   <>
+
   <div class="absolute top-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
   <div><Navbar/></div>
   <div><MainContent/></div>
