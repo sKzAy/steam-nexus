7d88f5af0ca4d34b8c97b2318e9c294eab80a4c9
sKzAy <1mkzaexe@gmail.com>
2024-10-13 08:14:01 +0500

    padding

--- components/registration.js
+++ components/registration.js
@@ -6,13 +6,13 @@ const Registration = () => {
     return (
         <>
             <div id='registration' className="about-event flex1 mt-10 ">
-                <div className=" mod-div text-white rounded-xl border-2 border-red-500 p-10"><h2 className='font-extrabold text-2xl text-yellow-500' id='modules size-large'>Registration:</h2>
-                    <h3 className='text-red-500 pb-4 pt-4 size-mid'>Registration fee: </h3>
+                <div className=" mod-div text-white rounded-xl border-2 border-red-500 p-10"><h2 className='font-extrabold text-2xl text-yellow-500' id='modules size-mid'>Registration:</h2>
+                    <h3 className='text-red-500 pb-4 pt-4 size-semismall'>Registration fee: </h3>
                     <ul>
                         <li className='size-small'> Fee Per Delegate: Rs.2000+2000 (Early Bird)</li>
                         <li className='size-small'>Fee Per Delegate: Rs.2500 + 2500 (Normal Registrations)</li>
                     </ul>
-                    <h3 className='text-red-500 pt-4 pb-4 size-mid'>Registration Details:</h3>
+                    <h3 className='text-red-500 pt-4 pb-4 size-semismall'>Registration Details:</h3>
                     <ul>
                         <li className='size-small'> Each delegation would comprise of 2-6 delegates</li>
                         <li className='size-small'>Registrations open: 11th October</li>
