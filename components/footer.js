c7169996d75607fc0bb10a677e21dbf89af0e92d
sKzAy <1mkzaexe@gmail.com>
2024-10-13 07:20:16 +0500

    tried

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,10 @@
+"use client"
 
 import React from 'react'
-
 const Footer = () => {
   return (
     <div className='flex1 mt-10'>
-         <div className="bottom-fixed text-white text-center w-full ">
+         <div className="bottom-fixed hide text-white text-center w-full ">
         Made by <a className='underline hover:text-lg ' target='_blank' href="https://www.instagram.com/s_kazimali_/profilecard/?igsh=MTNzcnJieXA5MTd1Zg==">Kazim Ali</a> 
         </div>
       
