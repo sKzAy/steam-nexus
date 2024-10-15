566a8be53c407996bf77b1b1e9572ec38c7d1ac4
sKzAy <1mkzaexe@gmail.com>
2024-10-15 14:14:31 +0500

    yes

--- components/ui/timeline.jsx
+++ components/ui/timeline.jsx
@@ -42,7 +42,7 @@ export const Timeline = ({
           <div  key={index} className="flex justify-start pt-10 md:pt-40 md:gap-10">
             <div data-aos="fade-up"
               className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div data-aos="fade-up"
+              <div data-aos="zoom-in"
                 className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black dark:bg-black flex items-center justify-center">
                 <div 
                   className="h-4 w-4 rounded-full bg-red-700 dark:bg-neutral-800 border border-red-700 dark:border-neutral-700 p-2" />
